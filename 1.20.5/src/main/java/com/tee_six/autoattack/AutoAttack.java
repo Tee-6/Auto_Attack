5f331b2b3a7c6653d9981ba0bff2b6d644292e59
Tommy Vo <130311928+Tee-6@users.noreply.github.com>
2024-04-23 19:03:29 -0700

    1.20.5 Update

---
+++ 1.20.5/src/main/java/com/tee_six/autoattack/AutoAttack.java
@@ -0,0 +1,29 @@
+package com.tee_six.autoattack;
+
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.fabricmc.api.EnvType;
+
+@Environment(EnvType.CLIENT)
+public class AutoAttack implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (client.options.attackKey.isPressed() && client.player != null && client.player.getAttackCooldownProgress(0) >= 1) {
+				if (client.crosshairTarget != null && client.crosshairTarget.getType() == HitResult.Type.ENTITY) {
+					Entity entity = ((EntityHitResult)client.crosshairTarget).getEntity();
+					if (entity.isAlive() && entity.isAttackable()) {
+						client.interactionManager.attackEntity(client.player, entity);
+						client.player.swingHand(Hand.MAIN_HAND);
+					}
+				}
+			}
+		});
+	}
+}
